c795dcb6eb55962fa4826f9454bd2b017fe6c909
oncee <onesick@iium.kr>
2023-04-10 15:16:43 +0900

    게시물 등록페이지 완료

--- pages/board/new/index.js
+++ pages/board/new/index.js
@@ -1,4 +1,3 @@
-import TextArea from 'antd/es/input/TextArea'
 import { 
   Container, 
   Wrapper, 
@@ -7,12 +6,19 @@ import {
   Child, 
   Label, 
   Input, 
-  Textar
+  Textar,
+  Zipcode,
+  ZipcodeInput,
+  SearchButton,
+  UploadButton,
+  RadioButton,
+  RadioLabel,
+  SubmitButton
 } from '../../../styles/emotion'
 
 export default function BoardNewPage() {
   return (
-    <div>
+    <>
       <Container>
         <Wrapper>
           <Title>게시물 등록</Title>
@@ -39,8 +45,41 @@ export default function BoardNewPage() {
               <Textar cols="30" rows="10" placeholder="내용을 작성해주세요." />
             </Child>
           </Group>
+          <Group mt={16}>
+            <Child>
+              <Label for="">주소</Label>
+              <Zipcode type="text" placeholder="07250" />
+              <SearchButton>우편번호 검색</SearchButton>
+              <ZipcodeInput type="text" />
+              <ZipcodeInput type="text" />
+            </Child>
+          </Group>
+          <Group mt={40}>
+            <Child>
+              <Label for="">유튜브</Label>
+              <Input type="text" placeholder="링크를 복사해주세요." />
+            </Child>
+          </Group>
+          <Group mt={40}>
+            <Child>
+              <Label for="">사진 첨부</Label>
+              <UploadButton>+</UploadButton>
+              <UploadButton>+</UploadButton>
+              <UploadButton>+</UploadButton>
+            </Child>
+          </Group>
+          <Group mt={40}>
+            <Child>
+              <Label for="">메인 설정</Label>
+              <RadioButton type="radio" id="youtube" name="radio-button" />
+              <RadioLabel htmlFor="youtube">유튜브</RadioLabel>
+              <RadioButton type="radio" id="image" name="radio-button" />
+              <RadioLabel htmlFor="image">사진</RadioLabel>
+            </Child>
+          </Group>
+          <SubmitButton>등록하기</SubmitButton>
         </Wrapper>
       </Container>
-    </div>
+    </>
   )
 }
--- styles/emotion.js
+++ styles/emotion.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 export const Container = styled.div`
   padding-top: 60px;
   padding-bottom: 100px;
+  font-family: 'Noto Sans CJK KR', sans-serif;
 `
 
 export const Wrapper = styled.div`
@@ -26,6 +27,8 @@ export const Group = styled.div`
   display: flex;
   flex-wrap: nowrap;
   flex-direction: row;
+  justify-content: center;
+  align-content: center;
   align-item: center;
   gap: 10px 24px;
   margin-top: ${props => (props.mt ? props.mt+'px' : '0')};
@@ -47,6 +50,7 @@ export const Input = styled.input`
   font-size: 16px;
   font-weight: 500;
   color: #000;
+  box-sizing: border-box;
 `
 
 export const Textar = styled.textarea`
@@ -57,4 +61,80 @@ export const Textar = styled.textarea`
   font-size: 16px;
   font-weight: 500;
   color: #000;
+  box-sizing: border-box;
 `
+
+export const Zipcode = styled.input`
+  width: 77px;
+  height: 52px;
+  padding-left: 16px;
+  border: 1px solid #bdbdbd;
+  box-sizing: border-box;
+  vertical-align: middle;
+`;
+
+export const ZipcodeInput = styled.input`
+  width: 100%;
+  height: 52px;
+  margin-top: 16px;
+  padding: 14px 16px;
+  border: 1px solid #bdbdbd;
+  font-size: 16px;
+  font-weight: 500;
+  color: #000;
+  box-sizing: border-box;
+`
+
+export const SearchButton = styled.button`
+  width: 124px;
+  height: 52px;
+  border: 1px solid #000;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  background-color: #000;
+  box-sizing: border-box;
+  vertical-align: middle;
+  margin-left: 16px;
+  font-family: 'Noto Sans CJK KR', sans-serif;
+`
+
+export const UploadButton = styled.button`
+  width: 78px;
+  height: 78px;
+  background-color: #eee;
+  margin-right: 24px;
+  font-size: 30px;
+  color: #666;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  font-family: 'Noto Sans CJK KR', sans-serif;
+`;
+
+export const RadioButton = styled.input`
+  cursor: pointer;
+`;
+
+export const RadioLabel = styled.label`
+  margin-left: 8px;
+  margin-right: 20px;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+export const SubmitButton = styled.button`
+  display: block;
+  width: 179px;
+  height: 52px;
+  border: none;
+  font-size: 16px;
+  font-weight: 500;
+  color: #000;
+  margin: 80px auto 0;
+  cursor: pointer;
+  background-color: #FFD600;
+  box-sizing: border-box;
+  font-family: 'Noto Sans CJK KR', sans-serif;
+`;
